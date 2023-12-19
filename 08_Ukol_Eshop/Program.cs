4890af7ac2e8de9557a716120f9e929d31b902f1
Emilie Kratochvilova <emilyk166@gmail.com>
2023-12-19 21:01:28 +0100

    Message added

--- 08_Ukol_Eshop/Program.cs
+++ 08_Ukol_Eshop/Program.cs
@@ -5,6 +5,11 @@ using System.Threading.Channels;
 using _08_Ukol_Eshop;
 using _08_Ukol_Eshop.Clothes;
 
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("SLOVO NA UVOD");
+Console.ResetColor();
+
+Console.WriteLine("Ahoj David,\naz se na tohle budes koukat, mohl bys mi prosim zkritizovat, co se da? Kdyz mi vytknes sebemensi blbost, tak budu rada - at se muzu poucit z chyb.\nDiky moc a mej se hezky!");
 
 Logic logic = new Logic();
 logic.ShowMainMenu();
